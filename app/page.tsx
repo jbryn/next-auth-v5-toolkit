f82633a0ee2d20cf98133fb9f55c8d591e92f60f
Jan Bryński <brynski.jan@gmail.com>
2024-04-19 16:12:18 +0200

    wip: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -16,9 +17,11 @@ export default function Home() {
         </h1>
         <p className="text-white text-lg">A simple auth service</p>
         <div>
-          <Button variant="secondary" size="lg">
-            Sign in
-          </Button>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
         </div>
       </div>
     </main>
